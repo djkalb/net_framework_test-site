91041583287b8f1f936a6491a290657f609e2ccd
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-21 20:28:18 -0800

    created little site with vid and animation

---
+++ dummy_site/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+let start = Date.now(); // remember start time
+
+let timer = setInterval(function () {
+    console.log('here 2')
+    // how much time passed from the start?
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 5000) {
+        clearInterval(timer); // finish the animation after 5 seconds
+        return;
+    }
+
+    // draw the animation at the moment timePassed
+    draw(timePassed);
+
+}, 20);
+
+
+function draw(timePassed) {
+    
+    document.getElementById("myAnimation").style.width = timePassed / 5 + '%';
+    console.log(document.getElementById("myAnimation").style.width)
+    document.getElementById("myAnimation").style.height = timePassed / 5 + '%';
+}
